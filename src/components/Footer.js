b0302f22ed7e8ca1c6caee8533595d3bde39d9ae
Jaybee <jubril_tunde@yahoo.com>
2024-03-28 13:50:55 +0100

    account details update

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,20 +4,17 @@ export default function Footer() {
   return (
     <>
       <footer className="footer border-t-2 border-gray-300 pt-5">
-        <ul className="flex flex-wrap items-center justify-center">
+        <ul className=" items-center justify-center">
           <li>
-            <span className="font-bold">Bank Name:</span> United Bank for Africa (UBA)
+            <span className="font-bold">Bank Name:</span> Moniepoint
           </li>
           <li>
-            <span className="font-bold">Account Name:</span> Tunde Jubril Sanusi
+            <span className="font-bold">Account Name:</span> Quench Fusion
           </li>
           <li>
-            <span className="font-bold">Account number:</span> 2055323091
+            <span className="font-bold">Account number:</span> 8065477475
           </li>
          
-          <li>
-            <span className="font-bold">Phone number:</span> +2348132991721
-          </li>
         </ul>
       </footer>
     </>
--- src/components/MainDetails.js
+++ src/components/MainDetails.js
@@ -5,7 +5,8 @@ export default function MainDetails() {
     <>
       <section className="flex flex-col items-end justify-end">
         <h2 className="font-bold text-3xl uppercase mb-1">Quench Fusion</h2>
-        <p>+2348132991721</p>
+        <p>Healthy Drinks</p>
+        <p>08065477475</p>
       </section>
     </>
   );
